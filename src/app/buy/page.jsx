0f1d2a88709cc529eb5b87aa83993129ec5e7d5d
lrayak09 <41226510+lrayak09@users.noreply.github.com>
2024-06-10 12:22:51 +0530

    UI update

--- src/app/buy/page.jsx
+++ src/app/buy/page.jsx
@@ -4,42 +4,46 @@ import BuySlide from '../components/BuySlide/BuySlide'
 
 import Header from '../components/Header/Header'
 const page = () => {
-  return (
-<>
-<Header/>
+    return (
+        <>
+            <Header />
 
-<section>
-    <div className="container ">
-        <div className="row align-items-center justify-content-center banner-buy buy-banner">
-            <div className="col-md-7">
-               <h1 className='text-white'>Trusted Place to Find a Home</h1>
-                 
-                 <div className="nav-box">
-                    <ul className='p-0 d-flex'>  
-                        <li>  <Link href="/">Buy</Link></li>
-                        <li> <Link href="rent">Rent</Link> </li>
-                        <li> <Link href="commercial">Commercial</Link> </li>
-                        <li> <Link href="lands">Lands</Link> </li>
+            <section>
+                <div className="container ">
+                    <div className="row align-items-center justify-content-center banner-buy buy-banner">
+                        <div className="col-md-7">
+                            <h1 className='text-white'>Trusted Place to Find a Home</h1>
 
-                    </ul>
-                 </div>
-                 <div className="serch-box d-flex">
-                        <button>select city</button>
-                        <button className='search-btn'>Search</button>
+                            <div className="nav-box">
+                                <ul className='p-0 d-flex'>
+                                    <li>  <Link href="/">Buy</Link></li>
+                                    <li> <Link href="rent">Rent</Link> </li>
+                                    <li> <Link href="commercial">Commercial</Link> </li>
+                                    <li> <Link href="lands">Lands</Link> </li>
 
+                                </ul>
+                            </div>
+                            <div className="serch-box d-flex">
+                                <select class="form-select border-0" aria-label="Default select example">
+                                    <option selected>Select City</option>
+                                    <option value="1">Indore</option>
+                                    <option value="2">Bhopal</option>
+                                </select>
+                                <button className='search-btn'>Search</button>
+
+                            </div>
+
+                            <div className="box-ex text-center">
+                                <span>Are you an Owner?<u> Post property for free</u></span>
+                            </div>
+                        </div>
                     </div>
+                </div>
+            </section>
 
-                    <div className="box-ex text-center">
-                        <span>Are you an Owner?<u> Post property for free</u></span>
-                    </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<BuySlide/>
-</>
-  )
+            <BuySlide />
+        </>
+    )
 }
 
 export default page
